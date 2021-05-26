bb113b2731bafec6aba5a9ed67fd0745950780c2
Santhosh Kumar <santhosh.prakash@gmail.com>
2021-05-26 16:29:48 +1000

    build: fix formatting hooks check

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+/* istanbul ignore next */
 const reportWebVitals = (onPerfEntry) => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
         import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
